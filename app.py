1ff4c2df1d285a586d05bda731abb19d15bb0be4
Juhan034 <juhan678000@gmail.com>
2025-05-29 21:17:03 +0900

    dummy change for PR

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.secret_key = 'secret-key'  # 세션용
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shoppingmall.db'
@@ -124,3 +125,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+# dummy
